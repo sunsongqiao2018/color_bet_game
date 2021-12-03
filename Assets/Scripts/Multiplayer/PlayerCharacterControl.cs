74c5a5d82fd04bac94616c278c2abf8b419fcb63
songqiao sun <sunsongqiao2015@gmail.com>
2021-12-03 12:03:38 -0500

    PlayerAnim Update

--- Assets/Scripts/Multiplayer/PlayerCharacterControl.cs
+++ Assets/Scripts/Multiplayer/PlayerCharacterControl.cs
@@ -8,50 +8,49 @@ using System;
 public class PlayerCharacterControl : MonoBehaviourPunCallbacks
 {
     [SerializeField] GameObject playerPrefab;
-    PlayerAnim playerCharAnim;
+    GameObject animatedPlayer;
     // Start is called before the first frame update
     // bool secondPlayerActive;
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
         photonView.RPC("AddPlayerCharacter", RpcTarget.Others);
-        HookPlayEvents(true);
+        //HookPlayEvents(true);
     }
     public override void OnPlayerLeftRoom(Player otherPlayer)
     {
-        if (playerCharAnim != null) Destroy(playerCharAnim.gameObject);
-        HookPlayEvents(false);
+        if (animatedPlayer != null) Destroy(animatedPlayer);
+        //HookPlayEvents(false);
     }
-    private void HookPlayEvents(bool hook)
-    {
-        if (hook)
-        {
-            StateMachine.Instance.GameStarted += AllPlayerCheer;
-            StateMachine.Instance.BroadcastResult += PlayerReactResult;
-        }
-        else
-        {
-            StateMachine.Instance.GameStarted -= AllPlayerCheer;
-            StateMachine.Instance.BroadcastResult -= PlayerReactResult;
-        }
-    }
+    //private void HookPlayEvents(bool hook)
+    //{
+    //    if (hook)
+    //    {
+    //        StateMachine.Instance.GameStarted += AllPlayerCheer;
+    //        StateMachine.Instance.BroadcastResult += PlayerReactResult;
+    //    }
+    //    else
+    //    {
+    //        StateMachine.Instance.GameStarted -= AllPlayerCheer;
+    //        StateMachine.Instance.BroadcastResult -= PlayerReactResult;
+    //    }
+    //}
 
-    private void PlayerReactResult(object sender, BoolEventArgs e)
-    {
-        bool iwon = e.value;
-        //raise win/lose anim event 
-    }
-    private void AllPlayerCheer(object sender, EventArgs e)
-    {
-        //raise win/lose anim event 
-
-        playerCharAnim.CallPlayerAnim(PlayerAnim.ANIMSTATES.STATE_CHEER);
-    }
+    //private void PlayerReactResult(object sender, BoolEventArgs e)
+    //{
+    //    bool iwon = e.value;
+    //    //raise win/lose anim event 
+    //}
+    //private void AllPlayerCheer(object sender, EventArgs e)
+    //{
+    //    //raise win/lose anim event 
+    //    playerCharAnim.CallPlayerAnim(PlayerAnim.ANIMSTATES.STATE_CHEER);
+    //}
 
     [PunRPC]
     private void AddPlayerCharacter()
     {
         //instantiate "yourself" in the other client world
-        GameObject player = PhotonNetwork.Instantiate(playerPrefab.name, transform.position, Quaternion.identity);
-        playerCharAnim = player.GetComponent<PlayerAnim>();
+        animatedPlayer = PhotonNetwork.Instantiate(playerPrefab.name, transform.position, Quaternion.identity);
+        //playerCharAnim = player.GetComponent<PlayerAnim>();
     }
 }
--- Assets/Scripts/PlayerAnim.cs
+++ Assets/Scripts/PlayerAnim.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -20,28 +21,76 @@ public class PlayerAnim : MonoBehaviour
         STATE_CHEER
     }
     // Start is called before the first frame update
-    async void Start()
+    private void Awake()
+    {
+        HookPlayEvents(true);
+    }
+    void Start()
     {
         _animator = gameObject.GetComponent<Animator>();
         idleStatHash = _animator.GetCurrentAnimatorStateInfo(0).shortNameHash;
-        await TriggerIdleAnim();
+        StartCoroutine(IdleAnimCoroutine());
     }
-    private async Task TriggerIdleAnim()
+    private void OnDisable()
     {
-        //pause if not in idle state;
+        StopCoroutine(IdleAnimCoroutine());
+        HookPlayEvents(false);
+    }
+    private void HookPlayEvents(bool hook)
+    {
+        if (hook)
+        {
+            StateMachine.Instance.GameStarted += AllPlayerCheer;
+            StateMachine.Instance.CardRevealed += PlayerReactResult;
+        }
+        else
+        {
+            StateMachine.Instance.GameStarted -= AllPlayerCheer;
+            StateMachine.Instance.CardRevealed -= PlayerReactResult;
+        }
+    }
+    private void PlayerReactResult(object sender, BoolEventArgs e)
+    {
+        bool playerWin = e.value;
+        if (playerWin) CallPlayerAnim(ANIMSTATES.STATE_WIN);
+        else CallPlayerAnim(ANIMSTATES.STATE_LOSE);
+    }
+    private void AllPlayerCheer(object sender, EventArgs e)
+    {
+        //raise win/lose anim event 
+        CallPlayerAnim(ANIMSTATES.STATE_CHEER);
+    }
+    private IEnumerator IdleAnimCoroutine()
+    {
         while (_animator.GetCurrentAnimatorStateInfo(0).shortNameHash != idleStatHash)
         {
-            await Task.Yield();
+            yield return null;
         }
         while (elapsedTime < idleTimer)
         {
-            await Task.Yield();
+            yield return new WaitForEndOfFrame();
             elapsedTime += Time.deltaTime;
         }
-        _animator.SetTrigger("IdleAnim");
+        if (_animator != null) _animator.SetTrigger("IdleAnim");
         elapsedTime = 0;
-        await TriggerIdleAnim();
+        yield return IdleAnimCoroutine();
     }
+    //private async Task TriggerIdleAnim()
+    //{
+    //    //pause if not in idle state;
+    //    while (_animator.GetCurrentAnimatorStateInfo(0).shortNameHash != idleStatHash)
+    //    {
+    //        await Task.Yield();
+    //    }
+    //    while (elapsedTime < idleTimer)
+    //    {
+    //        await Task.Yield();
+    //        elapsedTime += Time.deltaTime;
+    //    }
+    //    if (_animator != null) _animator.SetTrigger("IdleAnim");
+    //    elapsedTime = 0;
+    //    await TriggerIdleAnim();
+    //}
     public void CallPlayerAnim(ANIMSTATES state)
     {
         string triggerStr = null;
